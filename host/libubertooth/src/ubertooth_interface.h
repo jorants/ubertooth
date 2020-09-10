27f33572ee0e10b30d303503de7d133979f9eaa9
Joran van Apeldoorn <git@bitofbytes.com>
2020-09-10 19:21:35 +0200

    Longer packages (by or-zusman)

--- host/libubertooth/src/ubertooth_interface.h
+++ host/libubertooth/src/ubertooth_interface.h
@@ -121,8 +121,8 @@ enum rfcat24_subcommands {
 	RFCAT_CAP_LEN                = 2,
 };
 
-// maximum adv data len: 32 - (2 + 6 + 3)
-#define LE_ADV_MAX_LEN 21
+// maximum adv data len:
+#define LE_ADV_MAX_LEN 255
 
 enum jam_modes {
 	JAM_NONE       = 0,
